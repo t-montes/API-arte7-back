232e7dbc2c1bdbebbc2620efc75ebeb994f41ad0
t-montes <t.montes@uniandes.edu.co>
2022-09-23 10:18:11 -0500

    fix youtube-trailer dependency in movie service

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import MovieService from './movie.service';
 import MovieEntity from './movie.entity';
+import YoutubeTrailerEntity from '../youtube-trailer/youtube-trailer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieEntity])],
+  imports: [TypeOrmModule.forFeature([MovieEntity, YoutubeTrailerEntity])],
   providers: [MovieService]
 })
 export class MovieModule { }
